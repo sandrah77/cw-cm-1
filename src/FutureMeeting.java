550bfcb50ad3808c1cf3774f25ce3d28f74dd32b
Stephen Farmer <safarmer@icloud.com>
2015-12-15 19:11:37 +0000

    Creating interfaces and pushing first set of files. Needs structuring.

---
+++ src/FutureMeeting.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Alexander on 15/12/2015.
+ * A meeting to be held in the future
+ */
+public interface FutureMeeting extends Meeting{
+// No methods here, this is just a naming interface
+// (i.e. only necessary for type checking and/or downcasting)
+}
