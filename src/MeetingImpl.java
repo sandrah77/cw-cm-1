5657aa4be8cecb05d9a535ca985c73d9cb19e091
Stephen Farmer <safarmer@icloud.com>
2015-12-24 13:27:08 +0000

    Added constructor (included default for readability) and instance variables

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -1,6 +1,34 @@
+import java.util.Calendar;
+
 /**
  * Created by Alexander on 24/12/2015.
  */
-public abstract class MeetingImpl {
-    String
+public abstract class MeetingImpl implements Meeting {
+    private int ID;
+    private String date;
+    private Calendar date;
+    private Set<? extends Contact> contacts;
+
+    /**
+     * Adding default constructor for brevity
+     *
+     */
+    MeetingImpl(){
+    }
+
+    /**
+     * Constructor
+     *
+     */
+    MeetingImpl(int id, Calendar date, Set<Contact> contacts){
+        this.id = id;
+        this.date = date;
+        this.contacts = contacts;
+
+    }
+
+
+
+
+
 }
