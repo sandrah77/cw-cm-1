ddd0bbb9b7106d1a8dadd2a9bba8e3729f6354da
Stephen Farmer <safarmer@icloud.com>
2015-12-24 13:35:33 +0000

    Fixed instance variable, added parameters to constructor method

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -4,21 +4,26 @@ import java.util.Calendar;
  * Created by Alexander on 24/12/2015.
  */
 public abstract class MeetingImpl implements Meeting {
-    private int ID;
+    private int id;
     private String date;
     private Calendar date;
     private Set<? extends Contact> contacts;
 
     /**
      * Adding default constructor for brevity
-     *
      */
     MeetingImpl(){
     }
 
     /**
-     * Constructor
+     * Constructor with three parameters.
+     * The set of contacts must be non-empty or an IllegalArgumentException will be thrown
+     * An IllegalArgumentException will be thrown in the case of the id being non-positive or zero
+     * If any of the references/pointers passed as parameters is null, a NullPointerException is thrown
      *
+     * @param id the contact id
+     * @param date the date of the meeting
+     * @param contacts the list of people that attended the meeting
      */
     MeetingImpl(int id, Calendar date, Set<Contact> contacts){
         this.id = id;
