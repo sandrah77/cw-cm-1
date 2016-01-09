b32a975e716f832d3355d60cc6e2057b2fabde9c
Stephen Farmer <safarmer@icloud.com>
2016-01-09 16:18:28 +0000

    Using Before Class in PastMeetingImplTest to show notification

--- src/PastMeetingImplTest.java
+++ src/PastMeetingImplTest.java
@@ -8,7 +8,16 @@ import java.util.IllegalFormatCodePointException;
 import static org.junit.Assert.*;
 
 
+
 public class PastMeetingImplTest {
+
+        @BeforeClass
+        public static void runningNotification() {
+            System.out.println("Running PastMeetingImpl tests...");
+        }
+
+        
+
 
 
 }
