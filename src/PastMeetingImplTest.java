174c3671d0cd36ba41277338a09964df6b071c2d
Stephen Farmer <safarmer@icloud.com>
2016-01-09 16:38:45 +0000

    Added init() @Before: Creates HashSetand contacts are added to the set using ContactImpl

--- src/PastMeetingImplTest.java
+++ src/PastMeetingImplTest.java
@@ -2,21 +2,34 @@
  * Created by Alexander on 08/01/2016.
  */
 import org.junit.*;
-
-import java.util.IllegalFormatCodePointException;
-
-import static org.junit.Assert.*;
-
+import java.util.Set;
+import java.util.HashSet;
 
 
 public class PastMeetingImplTest {
 
-        @BeforeClass
-        public static void runningNotification() {
-            System.out.println("Running PastMeetingImpl tests...");
-        }
+    private Set<ContactImpl> contacts;
+    private ContactImpl contact1;
+    private ContactImpl contact2;
+    private ContactImpl contact3;
 
-        
+
+    @BeforeClass
+    public static void runningNotification() {
+        System.out.println("Running PastMeetingImpl tests...");
+    }
+
+    @Before
+    public void init() {
+        contacts = new HashSet<ContactImpl>();
+        contacts.add(new ContactImpl(1, "Matthew"));
+        contacts.add(new ContactImpl(3, "Philip"));
+        contacts.add(new ContactImpl(5, "James, the elder"));
+    }
+
+
+
+
 
 
 
