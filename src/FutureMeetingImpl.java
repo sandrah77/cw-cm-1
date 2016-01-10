05e5962a6759c18bbab7b6f11052dc3db2cb1df1
Stephen Farmer <safarmer@icloud.com>
2016-01-10 14:21:21 +0000

    Fixing constructor parameters

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -2,10 +2,22 @@
   * Created by Alexander on 24/12/2015.
   */
 
- public class FutureMeetingImpl {
+ public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
     /**
      * A class that implements the FutureMeeting interface
      *
      * @author sfarme01
+     *
      */
+
+
+     /**
+     *Constructor
+      *
+      * @param id the contact id
+      * @param date the date of the meeting
+      * @param contacts the list of people that attended the meeting
+      *                 
+     */
+
 }
